6426b39421417b176a5d08977fb549235e0cfade
Elias <e.duvernois2016@gmail.com>
2026-05-26 00:09:13 +0200

    ajout des fichiers js et installations des dépendances GSAP

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import './styles/main.css';
-import './styles/layout.css';
-import './styles/modules.css';
+import "./styles/main.css";
+import "./styles/layout.css";
+import "./styles/modules.css";
 
-import { initParticlesSection } from './modules/particles.js';
+import { initParticlesSection } from "./modules/particles.js";
 
-const featuresCanvas = document.getElementById('projects-bg');
-const featuresSection = document.getElementById('projects');
+const featuresCanvas = document.getElementById("projects-bg");
+const featuresSection = document.getElementById("projects");
 
 if (featuresCanvas && featuresSection) {
   initParticlesSection(featuresCanvas, featuresSection);
