b6b9485b76ce362569c679ee09a4dc54ceefd40b
Lytween Victoire <victoire.lytween@gmail.com>
2026-05-25 21:53:41 +0200

    partie hero +  nav

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
-import './styles/main.css';
-import './styles/layout.css';
-import './styles/modules.css';
+import "./styles/main.css";
+import "./styles/layout.css";
+import "./styles/modules.css";
+import { initHeroSection } from "./modules/hero.js";
+import { initParticlesSection } from "./modules/particles.js";
 
-import { initParticlesSection } from './modules/particles.js';
+const heroCanvas = document.getElementById("hero_background");
+const featuresCanvas = document.getElementById("projects-bg");
+const featuresSection = document.getElementById("projects");
 
-const featuresCanvas = document.getElementById('projects-bg');
-const featuresSection = document.getElementById('projects');
-
+if (heroCanvas) {
+  initHeroSection(heroCanvas);
+}
 if (featuresCanvas && featuresSection) {
   initParticlesSection(featuresCanvas, featuresSection);
 }
